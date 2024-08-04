3ce98d463cae0cbee16f4e13bd6376ea36d44562
yongjun96 <practice960426@gmail.com>
2024-08-04 13:31:45 +0900

    FEAT : 코틀린의 제어문 사용법
    3. switch와 when의 차이점과 when의 특징 알아보기 / when은 case를 사용하지 않고 ->를 통해서 값을 바로 작성할 수 있고, when 자체를 return 할 수 있다. 조건문에 in이나 is를 사용해서 유연하게 사용 가능하며 when의 값을 넣지 않더라도 조건문의 값을 확인하여 when을 return할 수 있다.

--- src/main/kotlin/com/lannstark/lec05/Lec05Main.kt
+++ src/main/kotlin/com/lannstark/lec05/Lec05Main.kt
@@ -32,3 +32,45 @@ fun getGrade(score: Int): String{
         "D"
     }
 }
+
+fun getGradeWithSwitch(score: Int): String {
+   return when (score / 10) {
+       9 -> "A"
+       8 -> "B"
+       7 -> "C"
+       else -> "D"
+   }
+}
+
+
+fun getGradeWithSwitch2(score: Int): String {
+    return when (score) {
+        in 90..99 -> "A"
+        in 80..89 -> "B"
+        in 70..79 -> "C"
+        else -> "D"
+    }
+}
+
+fun startsWithA(obj: Any): Boolean {
+    return when(obj){
+        is String -> obj.startsWith("A")
+        else -> false
+    }
+}
+
+
+fun judgeNumber(number: Int) {
+    when(number) {
+        1, 0, -1 -> println("어디서 많이 본 숫자입니다.")
+        else -> println("1, 0, -1이 아닙니다.")
+    }
+}
+
+fun judgeNumber2(number: Int) {
+    when{
+        number == 0 -> println("주어진 숫자는 0입니다")
+        number % 2 == 0 -> println("주어진 숫자는 짝수입니다")
+        else -> println("주어진 숫자는 홀수입니다")
+    }
+}
