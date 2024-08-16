b716a1d4d08c01b9dda98255f8916311b46ba89a
yongjun96 <practice960426@gmail.com>
2024-08-16 22:15:26 +0900

    FEAT : Kotlin의 클래스 다루기
    1-2. Java class를 사용하는 경우에도 .field를 사용할 수 있다. Kotlin은 기본적으로 setter를 가지고 있다고 보면 되기 때문에 .field를 setter 처럼 사용 가능

--- src/main/kotlin/com/lannstark/lec09/Person.kt
+++ src/main/kotlin/com/lannstark/lec09/Person.kt
@@ -1,5 +1,22 @@
 package com.lannstark.lec09
 
+import com.lannstark.lec01.Person
+
+
+fun main(){
+    val person = Person("김용준", 100)
+    println(person.name)
+    // 바로 값을 넣을 수 있다. (setter)
+    person.age = 10
+    println(person.age)
+
+    // Java class 를 가져 와서 쓴다고 해도 .field 를 사용할 수 있다.
+    val javaPerson = JavaPerson("김용준", 100)
+    println(javaPerson.name)
+    javaPerson.age = 10
+    println(javaPerson.age)
+}
+
 // Kotlin 은 생성자(constructor)를 class 를 생성할 때 지정
 // constructor 는 생략 가능
 // 또한 생성자에서 프로퍼티를 만들 수 있기 때문에 필드를 생략할 수 있다.
