42e643e625a8cbf4b947844dd7de45d5add7dfd2
yongjun96 <practice960426@gmail.com>
2024-08-18 21:40:39 +0900

    FEAT : Kotlin의 클래스 다루기

--- src/main/kotlin/com/lannstark/lec09/Person.kt
+++ src/main/kotlin/com/lannstark/lec09/Person.kt
@@ -1,8 +1,5 @@
 package com.lannstark.lec09
 
-import com.lannstark.lec01.Person
-
-
 fun main(){
     val person = Person("김용준", 100)
     println(person.name)
@@ -10,11 +7,25 @@ fun main(){
     person.age = 10
     println(person.age)
 
+    println("---------------------------------------------------------------")
+
     // Java class 를 가져 와서 쓴다고 해도 .field 를 사용할 수 있다.
     val javaPerson = JavaPerson("김용준", 100)
     println(javaPerson.name)
     javaPerson.age = 10
     println(javaPerson.age)
+
+    println("---------------------------------------------------------------")
+
+    // 새로운 생성자를 이용해서  name 만 입력 받고 age 는 default
+    val personV2 = Person("김용준")
+    println(personV2.name)
+    println(personV2.age)
+
+    println("---------------------------------------------------------------")
+
+
+    val personV3 = Person()
 }
 
 // Kotlin 은 생성자(constructor)를 class 를 생성할 때 지정
@@ -29,7 +40,33 @@ class Person (
         if( age <= 0){
             throw IllegalArgumentException("나이는 ${age}일 수 없습니다");
         }
+        println("초기화 블럭")
     }
+
+    // 새로운 생성자
+    constructor(name: String): this(name, 1) {
+        println("첫 번째 부 생성자")
+    }
+
+    constructor(): this("홍길동") {
+        println("두 번째 부 생성자")
+    }
+
+    // 전형적인 방식
+    fun isAdult(): Boolean {
+        return this.age >= 20
+    }
+
+    // custom getter
+    val isAdultV2: Boolean
+        get() = this.age >= 20
+
+    // (custom getter)프로퍼티
+    val isAdultV3: Boolean
+        get() {
+            return age >= 20
+        }
+
 }
 
     // {} -> body 또한 존재 하지 않는 다면 생략 가능
