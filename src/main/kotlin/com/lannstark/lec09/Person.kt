ea7f403585de6e71d9ba9f9cbf9342d3394bb9f8
yongjun96 <practice960426@gmail.com>
2024-08-19 22:09:24 +0900

    FEAT : Kotlin의 클래스 다루기
    2-2. constructor : 주 생성자(primary constructor)는 반드시 존재해야 하고 생성자의 파라미터가 하나도 없는 경우는 생략이 가능하다. 부 생성자는 this로 주 생성자를 무조건 호출해야 한다. / 하지만 코틀린은 부 생성자보다 default parameter를 권장하며 어쩔 수 없이 사용해야 하는 경우가 있다면 정적 팩토리 메서드를 사용하는 것을 권장한다.
    
    3. 커스텀 getter, setter : 함수를 만들어서 호촐하는 방식과 custom getter를 만들어서 호출하는 방식 두가지가 주로 쓰인다. 효과적으로 사용하려면 객체의 속성 >> custom getter / 그렇지 않은 경우 >> 함수
    
    4. backing field (보이지 않는 필드) : custom getter를 만들 때 해당하는 필드를 호출하게 되는데 class안에서 호출하게 되면 본인이 본인을 호출하는 무한 루프에 빠지기 때문에 field라는 예약어를 사용한다 / 하지만!!! this로 대체가 가능하기 때문에 잘 쓰지 않는다.

--- src/main/kotlin/com/lannstark/lec09/Person.kt
+++ src/main/kotlin/com/lannstark/lec09/Person.kt
@@ -32,9 +32,32 @@ fun main(){
 // constructor 는 생략 가능
 // 또한 생성자에서 프로퍼티를 만들 수 있기 때문에 필드를 생략할 수 있다.
 class Person (
-    val name: String,
-    var age: Int
+    name: String = "김용준",
+    var age: Int = 1
 ){
+
+    // 원래 사용하던 val name: String = "김용준" 은 custom getter를 만들기 위해 아래로 내려서 get()으로 써준다
+    // field. 을 사용하는 이유 : 밖에서 호출할 때는 get()을 부르고 name이라는 필드가 먼저 호출
+    // 안에서도 name을 호출하면 name의 대한 getter가 호출된다.
+    // 안에 있는 getter를 부르고 부르는 무한 루프가 발생하게 된다.
+    // 이것을 backing field (보이지 않는 필드)라고 부른다
+    // 중요 ※ : 하지만 사용할 일이 별로 없다. 이유는!!
+//    var name = name
+//        get() = field.uppercase()
+
+    // 이렇게 this를 사용해서 backing field를 대체할 수 있다.
+    val upperCaseName: String
+        get() = this.name.uppercase()
+
+    // custom setter
+    // value는 들어오는 파라미터를 대문자로 바꾸로 field(자기 자신)에게 넣어 준다
+    // 사실상 setter의 사용을 지양하기 때문에 쓸일이 없다
+    var name = name
+        set(value) {
+            field = value.uppercase()
+        }
+
+
     // class(생성되는 시점) 가 초기화 되는 시점에 한번 호출되는 블럭
     init {
         if( age <= 0){
