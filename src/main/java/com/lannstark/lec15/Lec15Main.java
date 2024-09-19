5b5acf2e730630360a12f13fb8159c89768dbae6
yongjun96 <practice960426@gmail.com>
2024-09-20 00:01:32 +0900

    FEAT : Kotlin의 배열과 컬렉션 다루기
    1. List : listOf로 생성 / empty의 타입이 추론 된다면 타입을 명시하지 않아도 된다. / 가변 list는 mutableListOf로 생성 / 불변 list를 먼저 먼들고 필요한 경우만 가변 사용 권장
    
    2. set : setOf로 생성 / list와 마찬가지로 for문을 사용가능 하고, mutableSetOf를 이용해 가변 set을 생성 가능 / 기본 구현체는 LinkedHashSet

--- src/main/java/com/lannstark/lec15/Lec15Main.java
+++ src/main/java/com/lannstark/lec15/Lec15Main.java
@@ -1,5 +1,8 @@
 package com.lannstark.lec15;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Lec15Main {
 
   public static void main(String[] args) {
@@ -8,6 +11,30 @@ public class Lec15Main {
     for(int i=0; i<array.length; i++) {
       System.out.println("순번 : "+ i + " / 값 : " + array[i]);
     }
+
+    //-----------------------------------------------------------------------------------------
+
+    // List
+
+    final List<Integer> numbers = Arrays.asList(100, 200);
+
+    // 하나 가져오기
+    System.out.println(numbers.get(0));
+
+    // For Each
+    for(int number : numbers){
+      System.out.println(number);
+    }
+
+    // 전통적인 For문
+    for(int i = 0; i < numbers.size(); i++){
+      System.out.printf("%s %s", i, numbers.get(i));
+    }
+
+    //-----------------------------------------------------------------------------------------
+
+    // Set
+
 
   }
 
--- src/main/kotlin/com/lannstark/lec15/Lec15Main.kt
+++ src/main/kotlin/com/lannstark/lec15/Lec15Main.kt
@@ -18,6 +18,57 @@ fun main(){
         println("순번 : ${index} / 값 : ${value}")
     }
 
+    //-----------------------------------------------------------------------------------------
 
+    // List
+
+    val numbers = listOf(100, 200)
+
+    // 비어 있는 리스트의 타입은 명시적으로 적어야 한다.
+    val emptyList = emptyList<Int>()
+
+    // 비어 있는 List를 넣더라도 Type 추론이 되기 때문에 가능하다.
+    printNumbers(emptyList())
+
+
+    // 배열처럼 대괄호로 가져올 수 있다.
+    println(numbers[0])
+
+    // for each문
+    for (number in numbers){
+        println(number)
+    }
+
+    // for withIndex()
+    for ((idx, value) in numbers.withIndex()) {
+        println("$idx $value")
+    }
+
+    // 가변 List
+    val numbersV2 = mutableListOf(400, 600)
+    numbersV2.add(300)
+
+    //-----------------------------------------------------------------------------------------
+
+    // Set
+    val setNumbers = setOf(100, 200)
+
+    // for each
+    for(number in setNumbers) {
+        println(number)
+    }
+
+    // for withIndex()
+    for ((index, value) in setNumbers.withIndex()) {
+        println("$index $value")
+    }
+
+    // Set의 가변(Mutable)
+    // 기본 구현체는 LinkedHashSet
+    val setNumbersV2 = mutableSetOf(400, 600)
+
+}
+
+private fun printNumbers(numbers: List<Int>){
 
 }
